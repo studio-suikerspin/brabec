07102562cc83b4cdd8b26013449790b8cacf0fea
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-08-23 19:35:19 +0200

    Setup BRABEC with Prismic

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+import { repositoryName, apiEndpoint } from './slicemachine.config.json';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'Prismic + Nuxt Minimal Starter',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    }
+  },
+
+  modules: ['@nuxt/eslint', '@nuxtjs/prismic', '@nuxt/fonts'],
+  
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  
+  css: ['~/assets/css/global.css'],
+
+  prismic: {
+    endpoint: apiEndpoint || repositoryName,
+    preview: '/api/preview',
+    clientConfig: {
+      routes: [
+        {
+          type: 'page',
+          path: '/:uid',
+        },
+        {
+          type: 'page',
+          uid: 'home',
+          path: '/',
+        },
+      ]
+    }
+  },
+
+  compatibilityDate: '2025-07-16',
+})
