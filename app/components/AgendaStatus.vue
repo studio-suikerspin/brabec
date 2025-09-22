11d132485adbd22a6ea158cccc35dcbcfac95e5f
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-09-22 21:10:48 +0200

    Components

---
+++ app/components/AgendaStatus.vue
@@ -0,0 +1,40 @@
+<script setup>
+const {agenda} = defineProps({
+  agenda: Object,
+}); 
+</script>
+
+<template>
+    <div class="agenda">
+      <div :class="`agenda__dot agenda__dot--${agenda.open ? 'green' : 'red'}`" />
+      <div class="agenda__text">
+        Agenda open - {{ agenda.spots_available }} {{ agenda.spots_text }}
+      </div>
+    </div>
+</template>
+
+<style lang="scss">
+.agenda {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  padding-inline: 16px;
+  padding-block: 12px;
+
+  border: 1px solid var(--accent_primary);
+  border-radius: 100px;
+
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  
+  &__dot {
+    width: 10px;
+    height: 10px;
+    border-radius: 100%;
+
+    &--green {
+      background: #10B70B;
+    }
+  }
+}
+</style>
---
+++ app/components/UI/Button.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  href: {
+    type: String,
+    default: 'javascript:;'
+  },
+  variant: {
+    type: String,
+    default: 'primary',
+  },  
+  classes: {
+    type: String,
+    default: '',
+  }
+})
+</script>
+
+<template>
+  <a :class="`btn btn--${variant} ${classes}`" :href="href">
+    <div class="btn__inner">
+      <slot />
+    </div>
+  </a>
+</template>
+
+<style lang="scss">
+.btn {
+  display: flex;
+  padding-inline: 12px;
+  padding-block: 8px;
+  gap: 10px;
+  border-radius: 50px;
+  background: rgba(255, 255, 255, 0.30);
+
+  font-weight: 600;
+  line-height: normal;
+
+  &__inner {
+    padding-inline: 24px;
+    padding-block: 12px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    border-radius: 50px;
+  }
+
+  &--primary {
+
+  }
+
+  &--white {
+    .btn__inner {
+      background: #fff;
+    }
+  }
+}
+</style>
