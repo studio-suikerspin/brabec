fc18a403dc938b3a881465267ecebf821c3d9ce4
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-11-06 13:30:21 +0100

    Add transition to titles

--- app/components/Section/Eyebrow.vue
+++ app/components/Section/Eyebrow.vue
@@ -1,38 +1,58 @@
+<script setup>
+const { gsap } = useGsap();
+
+const eyebrow = ref(null);
+
+onMounted(() => {
+  gsap.from(eyebrow.value, {
+    duration: 0.2,
+    autoAlpha: 0,
+    y: 40,
+    scrollTrigger: {
+      trigger: eyebrow.value.parentElement,
+      start: 'top 70%',
+      end: 'bottom 50%',
+      scrub: 0.5,
+    },
+  });
+});
+</script>
+
 <template>
-  <div class="section-eyebrow">
-    <div class="section-eyebrow__line"/>
+  <div ref="eyebrow" class="section-eyebrow">
+    <div class="section-eyebrow__line" />
     <div class="section-eyebrow__text"><slot /></div>
-    <div class="section-eyebrow__line"/>
+    <div class="section-eyebrow__line" />
   </div>
 </template>
 
 <style lang="scss">
-  .section-eyebrow {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 24px;
+.section-eyebrow {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
 
-    flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  width: 100%;
+
+  &__line {
     width: 100%;
+    height: 1px;
 
-    &__line {
-      width: 100%;
-      height: 1px;
-
-      max-width: 115px;
-      background-color: var(--black-20);
-    }
-
-    &__text {
-      color: var(--black);
-      text-align: center;
-      font-family: Inter;
-      font-size: 16px;
-      font-style: italic;
-      font-weight: 400;
-      line-height: normal;
-      text-wrap: nowrap;
-    }
+    max-width: 115px;
+    background-color: var(--black-20);
   }
+
+  &__text {
+    color: var(--black);
+    text-align: center;
+    font-family: Inter;
+    font-size: 16px;
+    font-style: italic;
+    font-weight: 400;
+    line-height: normal;
+    text-wrap: nowrap;
+  }
+}
 </style>
--- app/components/Section/Title.vue
+++ app/components/Section/Title.vue
@@ -1,5 +1,25 @@
+<script setup>
+const { gsap } = useGsap();
+
+const title = ref(null);
+
+onMounted(() => {
+  gsap.from(title.value, {
+    duration: 0.2,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: title.value.parentElement,
+      start: 'top 70%',
+      end: 'bottom 50%',
+      scrub: 0.5,
+    },
+  });
+});
+</script>
+
 <template>
-  <h2 class="section-title">
+  <h2 ref="title" class="section-title">
     <slot />
   </h2>
 </template>
